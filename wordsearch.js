c7532fd7efcdd9e4953f5f223b8b8b4157691fe5
Keith Millar <keithmillar@hotmail.ca>
2020-06-04 01:18:21 +0000

    looks for word in rows then transposes the matrix and looks in the new rows for the word

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,50 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  const newArray = [];
 
-module.exports = wordSearch
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArray.push([]);
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let k = 0; k < matrix[i].length; k++) {
+      newArray[k].push(matrix[i][k]);
+    }
+  }
+  
+  return newArray;
+};
+
+// const wordSearch = (letters, word) => {
+//  const horizontalJoin = letters.map(ls => ls.join(''))
+//   for (const l of horizontalJoin) {
+//         if (l.includes(word)) {
+//         return true;
+//     }
+// }
+//     return false
+// };
+
+
+
+const wordSearch = function(letters,word) {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  // transpose the matrix to search through the columns.
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  
+  
+  return false;
+};
+
+module.exports = wordSearch;
